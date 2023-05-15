5124678183ffe92472b3f44743610c5caa007c1f
fathbsyr <114373275+fathbsyr@users.noreply.github.com>
2023-05-15 15:07:03 +0700

    Upload UTS

---
+++ fronted-end/template/bawah_detail.php
@@ -0,0 +1,115 @@
+<div class="colorlib-partner">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-8 offset-sm-2 text-center colorlib-heading colorlib-heading-sm">
+				<h2>Trusted Partners</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col partner-col text-center">
+				<img src="../../images/pos.jpg" class="img-fluid" alt="Free html4 bootstrap 4 template">
+			</div>
+			<div class="col partner-col text-center">
+				<img src="../../images/JNE.png" class="img-fluid" alt="Free html4 bootstrap 4 template">
+			</div>
+			<div class="col partner-col text-center">
+				<img src="../../images/Gojek-Logo.png" class="img-fluid" alt="Free html4 bootstrap 4 template">
+			</div>
+			<div class="col partner-col text-center">
+				<img src="../../images/sicepat.jpg" class="img-fluid" alt="Free html4 bootstrap 4 template">
+			</div>
+			<div class="col partner-col text-center">
+				<img src="../../images/Jnt.png" class="img-fluid" alt="Free html4 bootstrap 4 template">
+			</div>
+		</div>
+	</div>
+</div>
+<footer id="colorlib-footer" role="contentinfo">
+	<div class="container">
+		<div class="row row-pb-md">
+			<div class="col footer-col colorlib-widget">
+				<h4>Tentang Kecantikan</h4>
+				<p> keindahan dan kecantikan itu memerlukan dukungan dari pikiran kita</p>
+				<p>
+				<ul class="colorlib-social-icons">
+					<li><a href="https://www.facebook.com/mubasyir.mubasyir.79"><i class="icon-facebook"></i></a></li>
+					<li><a href="https://www.linkedin.com/in/fatih-mubasyir-9b2625251/"><i class="icon-linkedin"></i></a></li>
+					<li><a href="https://www.instagram.com/fatih_basyir/"><i class="icon-instagram"></i></a></li>
+				</ul>
+				</p>
+			</div>
+			<div class="col footer-col colorlib-widget">
+				<h4>About As</h4>
+				<p>
+				<ul class="colorlib-footer-links">
+					<li><a href="../../page/utama/about.php">Kisah Kami</a></li>
+					<li><a href="../../page/utama/review.php">Review</a></li>
+				</ul>
+				</p>
+			</div>
+			<div class="col footer-col">
+				<h4>pay method</h4>
+				<ul class="colorlib-footer-links">
+					<li>Bank</li>
+					<li>E-Wallet</li>
+				</ul>
+			</div>
+
+			<div class="col footer-col">
+				<h4>Contact Information</h4>
+				<ul class="colorlib-footer-links">
+					<li>Perumahan CIbinong City, <br>16913</li>
+					<li>+628953XXXXXXXX</li>
+					<li>fatihmubasyir0@gmail.com</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="copy">
+		<div class="row">
+			<div class="col-sm-12 text-center">
+				<p>
+					<span><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+						Copyright &copy;<script>
+							document.write(new Date().getFullYear());
+						</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></span>
+					<span class="block">Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a> , <a href="http://pexels.com/" target="_blank">Pexels.com</a> and Develop By Fatih Mubasyir</span>
+				</p>
+			</div>
+		</div>
+	</div>
+</footer>
+</div>
+
+<div class="gototop js-top">
+	<a href="#" class="js-gotop"><i class="ion-ios-arrow-up"></i></a>
+</div>
+
+<!-- jQuery -->
+<script src="../../js/jquery.min.js"></script>
+<!-- popper -->
+<script src="../../js/popper.min.js"></script>
+<!-- bootstrap 4.1 -->
+<script src="../../js/bootstrap.min.js"></script>
+<!-- jQuery easing -->
+<script src="../../js/jquery.easing.1.3.js"></script>
+<!-- Waypoints -->
+<script src="../../js/jquery.waypoints.min.js"></script>
+<!-- Flexslider -->
+<script src="../../js/jquery.flexslider-min.js"></script>
+<!-- Owl carousel -->
+<script src="../../js/owl.carousel.min.js"></script>
+<!-- Magnific Popup -->
+<script src="../../js/jquery.magnific-popup.min.js"></script>
+<script src="../../js/magnific-popup-options.js"></script>
+<!-- Date Picker -->
+<script src="../../js/bootstrap-datepicker.js"></script>
+<!-- Stellar Parallax -->
+<script src="../../js/jquery.stellar.min.js"></script>
+<!-- Main -->
+<script src="../../js/main.js"></script>
+
+</body>
+
+</html>
